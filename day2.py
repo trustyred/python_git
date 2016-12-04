9048b669114f7391a74a8f826f048290ec7960f9
trustyred <244073249@qq.com>
2016-12-04 18:48:16 +0800

    add highlight search test

--- day2.py
+++ day2.py
@@ -18,3 +18,18 @@ for i in range(l.count('2')):
 	else :
 		first=l.index('2',first+1)
 	print first
+
+#高亮搜索的一个例子
+
+staff_data='./staff_data'
+
+f=file(staff_data,'r')
+while True:
+	info=raw_input("input search info: ")
+	for i in f.readlines():
+		if info in i.strip():
+			page=i.strip().replace(info,'\033[1;32m'+info+'\033[0m')
+			print page
+
+	f.seek(0)
+
